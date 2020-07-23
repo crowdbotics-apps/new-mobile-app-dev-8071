bea421309fe8ba361ff4d87d51874246215160ff
Crowdbotics <team@crowdbotics.com>
2020-07-23 19:16:26 +0000

    Update components

--- src/features/BlankScreen19309/screens/blank.js
+++ src/features/BlankScreen19309/screens/blank.js
@@ -61,40 +61,6 @@ export class _Blank extends React.Component {
         backgroundSize: "auto"
       }}
     >
-      <Text
-        style={{
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textShadow: "0pt 0pt 0pt #000000",
-          textDecoration: "none",
-          textTransform: "none",
-          lineHeight: 1
-        }}
-      >
-        Sample text content
-      </Text>
       <Button
         style={{
           width: "100%",
@@ -131,6 +97,36 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
+      <Text
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "center",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "dotted",
+          borderWidth: 4,
+          borderRadius: 5,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none",
+          lineHeight: 1
+        }}
+      >
+        Sample text content
+      </Text>
     </View>
   )
 }
